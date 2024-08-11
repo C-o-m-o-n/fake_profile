58db4a5c9c668d9f0df3e4b78e50ace645aa8bc9
C-o-m-o-n <comon928@gmail.com>
2024-08-11 18:04:43 +0300

    fix import issue

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import setup, find_packages
 
 setup(
     name='fake_profile',
-    version='0.1.0',
+    version='0.2.0',
     author='Collins O. Odhiambo',
-    description = "This is a python package that helps developers to generate fake social media user profiles and interactions for development purposes",
+    description = 'This is a python package that helps developers to generate fake social media user profiles and interactions for development purposes',
     author_email='comon928@gmail.com',
     url='https://github.com/C-o-m-o-n/fake_profile',
     packages=find_packages(),
