8da61e73bc631efd4a60691d1b5424acfe360208
C-o-m-o-n <comon928@gmail.com>
2024-08-11 17:43:32 +0300

    edit setup

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
+
 from setuptools import setup, find_packages
 
 setup(
     name='fake_profile',
     version='0.1.0',
-    description='A package to generate fake social media user data and interactions',
     author='Collins O. Odhiambo',
-    author_email='your.email@example.com',
+    description = "This is a python package that helps developers to generate fake social media user profiles and interactions for development purposes",
+    author_email='comon928@gmail.com',
     url='https://github.com/C-o-m-o-n/fake_profile',
     packages=find_packages(),
     install_requires=[
@@ -13,9 +14,9 @@ setup(
     ],
     classifiers=[
         'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: GNU General Public License',
         'Operating System :: OS Independent',
     ],
     python_requires='>=3.6',
 )
+
 
