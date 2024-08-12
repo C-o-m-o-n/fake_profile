a3d2ed5ed86760af47204590d8df5bd244a2ecee
o21ex <154024318+c2-tlhah@users.noreply.github.com>
2024-08-12 13:32:55 +0500

    Added Modeule to generate Fake Linkedin profiles.

--- fake_profile/generator.py
+++ fake_profile/generator.py
@@ -1,12 +1,10 @@
 from faker import Faker
 import random, json
 
-
 class Xprofile:
     def __init__(self):
         self.fake = Faker()
 
-    # Generate fake Twitter user data
     def generate_fake_user(self):
         return {
             "user_id": self.fake.uuid4(),
@@ -15,8 +13,8 @@ class Xprofile:
             "bio": self.fake.text(max_nb_chars=160),
             "location": self.fake.city(),
             "url": self.fake.url(),
-            "profile_image_url": "https://randomfox.ca/", #fake.image_url(width=400, height=400),
-            "banner_image_url": "https://randomfox.ca/", #fake.image_url(width=1500, height=500),
+            "profile_image_url": "https://randomfox.ca/",
+            "banner_image_url": "https://randomfox.ca/",
             "join_date": str(self.fake.date_time_this_decade()),
             "verified_status": self.fake.boolean(chance_of_getting_true=10),
             "follower_count": random.randint(0, 1000000),
@@ -31,7 +29,7 @@ class Xprofile:
             "default_profile": self.fake.boolean(chance_of_getting_true=30),
             "default_profile_image": self.fake.boolean(chance_of_getting_true=20)
         }
-    # Generate fake tweet data associated with a user
+
     def generate_fake_tweet(self, user_id):
         created_at = str(self.fake.date_time_between(start_date='-2y', end_date='now'))
         return {
@@ -47,9 +45,9 @@ class Xprofile:
             "is_reply": self.fake.boolean(chance_of_getting_true=15),
             "is_quoted": self.fake.boolean(chance_of_getting_true=10),
             "language": self.fake.language_code(),
-            "tweet_image_url": "https://randomfox.ca/", #fake.image_url(width=400, height=400),
+            "tweet_image_url": "https://randomfox.ca/",
         }
-    # Generate fake comments for a tweet
+
     def generate_fake_comment(self, tweet_id, user_id):
         return {
             "comment_id": self.fake.uuid4(),
@@ -58,7 +56,7 @@ class Xprofile:
             "text": self.fake.sentence(nb_words=15),
             "created_at": str(self.fake.date_time_between(start_date='-2y', end_date='now')),
         }
-    # Generate fake likes for a tweet
+
     def generate_fake_like(self, tweet_id, user_id):
         return {
             "like_id": self.fake.uuid4(),
@@ -66,7 +64,7 @@ class Xprofile:
             "user_id": user_id,
             "created_at": str(self.fake.date_time_between(start_date='-2y', end_date='now')),
         }
-    # Generate fake retweets for a tweet
+
     def generate_fake_retweet(self, tweet_id, user_id):
         return {
             "retweet_id": self.fake.uuid4(),
@@ -74,7 +72,7 @@ class Xprofile:
             "user_id": user_id,
             "created_at": str(self.fake.date_time_between(start_date='-2y', end_date='now')),
         }
-    # Generate a list of fake tweets and their interactions for a user
+
     def generate_fake_tweets_and_interactions(self, user_id, tweet_count=10):
         tweets = []
         comments = []
@@ -85,7 +83,6 @@ class Xprofile:
             tweet = self.generate_fake_tweet(user_id)
             tweets.append(tweet)
 
-            # Generate interactions for each tweet
             num_comments = random.randint(0, 10)
             num_likes = random.randint(0, 100)
             num_retweets = random.randint(0, 50)
@@ -95,7 +92,7 @@ class Xprofile:
             retweets.extend([self.generate_fake_retweet(tweet['tweet_id'], user_id) for _ in range(num_retweets)])
 
         return tweets, comments, likes, retweets
-    # Generate a list of fake Twitter users and their associated tweets and interactions
+
     def generate_fake_twitter_data(self, user_count=10, tweets_per_user=5):
         users = []
         all_tweets = []
@@ -116,97 +113,232 @@ class Xprofile:
             all_retweets.extend(user_retweets)
 
         return users, all_tweets, all_comments, all_likes, all_retweets
- 
-class InstaGram:
-  def __init__(self):
-    self.fake = Faker()
-    # Generate fake Twitter user data
-  def generate_user(self):
-    return {
-      "user_id": self.fake.uuid4(),
-      "username": self.fake.user_name(),
-      "full_name": self.fake.name(),
-      "bio": self.fake.text(max_nb_chars=160),
-      "profile_image_url": "https://randomfox.ca/",
-      "website": self.fake.url(),
-      "followers": random.randint(0, 1000000),
-      "following": random.randint(0, 1000),
+
+
+class InstaProfile:
+    def __init__(self):
+        self.fake = Faker()
+
+    def generate_user(self):
+        return {
+            "user_id": self.fake.uuid4(),
+            "username": self.fake.user_name(),
+            "full_name": self.fake.name(),
+            "bio": self.fake.text(max_nb_chars=160),
+            "profile_image_url": "https://randomfox.ca/",
+            "website": self.fake.url(),
+            "followers": random.randint(0, 1000000),
+            "following": random.randint(0, 1000),
         }
-  def generate_comment(self, post_id, user_id):
-    return {
-      "comment_id":self.fake.uuid4(),
-      "user": user_id,
-      "post": post_id,
-      "comment": self.fake.text(max_nb_chars=200),
-      "likes": random.randint(0, 50000),
-      "created_at": str(self.fake.date_time_between(start_date='-2y', end_date='now')),
-    }
-  # Generate fake likes for a post
-  def generate_like(self, post_id, user_id):
-    return {
-      "like_id": self.fake.uuid4(),
-      "post_id": post_id,
-      "user_id": user_id,
-      "created_at": str(self.fake.date_time_between(start_date='-2y', end_date='now')),
+
+    def generate_comment(self, post_id, user_id):
+        return {
+            "comment_id": self.fake.uuid4(),
+            "user": user_id,
+            "post": post_id,
+            "comment": self.fake.text(max_nb_chars=200),
+            "likes": random.randint(0, 50000),
+            "created_at": str(self.fake.date_time_between(start_date='-2y', end_date='now')),
         }
-  # Generate fake retweets for a post
-  def generate_repost(self, post_id, user_id):
-    return {
-      "retweet_id": self.fake.uuid4(),
-      "post_id": post_id,
-      "user_id": user_id,
-      "created_at": str(self.fake.date_time_between(start_date='-2y', end_date='now')),
+
+    def generate_like(self, post_id, user_id):
+        return {
+            "like_id": self.fake.uuid4(),
+            "post_id": post_id,
+            "user_id": user_id,
+            "created_at": str(self.fake.date_time_between(start_date='-2y', end_date='now')),
         }
-  def generate_post(self, user_id):
-    return {
-      "post_id": self.fake.uuid4(),
-      "image_url": "https://randomfox.ca/",
-      "caption": self.fake.text(),
-      "likes": random.randint(0, 50000),
+
+    def generate_repost(self, post_id, user_id):
+        return {
+            "retweet_id": self.fake.uuid4(),
+            "post_id": post_id,
+            "user_id": user_id,
+            "created_at": str(self.fake.date_time_between(start_date='-2y', end_date='now')),
         }
-  # Generate a list of posts and their interactions for a user
-  def generate_post_and_interactions(self, user_id, post_count=10):
-    posts = []
-    comments = []
-    likes = []
-    reposts = []
-    
-    for _ in range(post_count):
-      post = self.generate_post(user_id)
-      posts.append(post)
-      # Generate interactions for each post
-      num_comments = random.randint(0, 10)
-      num_likes = random.randint(0, 100)
-      num_reposts = random.randint(0, 50)
-      comments.extend([
-        self.generate_comment(post['post_id'], user_id) for _ in range(num_comments)
-        ])
-      likes.extend([self.generate_like(post['post_id'], user_id) for _ in range(num_likes)])
-      reposts.extend([self.generate_repost(post['post_id'], user_id) for _ in range(num_reposts)])
-      return posts, comments, likes, reposts
-    
-  # Generate a list of fake InstaGram users and their associated posts and interactions
-  def generate_fake_user_data(self, user_count=10, posts_per_user=5):
-      users = []
-      all_posts = []
-      all_comments = []
-      all_likes = []
-      all_reposts = []
 
-      for _ in range(user_count):
-          user = self.generate_user()
-          users.append(user)
+    def generate_post(self, user_id):
+        return {
+            "post_id": self.fake.uuid4(),
+            "image_url": "https://randomfox.ca/",
+            "caption": self.fake.text(),
+            "likes": random.randint(0, 50000),
+        }
 
-          user_posts, user_comments, user_likes, user_reposts = self.generate_post_and_interactions(user['user_id'], post_count=posts_per_user)
+    def generate_post_and_interactions(self, user_id, post_count=10):
+        posts = []
+        comments = []
+        likes = []
+        reposts = []
 
-          all_posts.extend(user_posts)
-          all_comments.extend(user_comments)
-          all_likes.extend(user_likes)
-          all_reposts.extend(user_reposts)
+        for _ in range(post_count):
+            post = self.generate_post(user_id)
+            posts.append(post)
 
-      return users, all_posts, all_comments, all_likes, all_reposts
+            num_comments = random.randint(0, 10)
+            num_likes = random.randint(0, 100)
+            num_reposts = random.randint(0, 50)
 
-#save the fake user profile to a file
-def save_user_data(fake_user,filename="fake_data.json"):
-  with open(filename, "w") as file:
-    json.dump(fake_user, file)
+            comments.extend([self.generate_comment(post['post_id'], user_id) for _ in range(num_comments)])
+            likes.extend([self.generate_like(post['post_id'], user_id) for _ in range(num_likes)])
+            reposts.extend([self.generate_repost(post['post_id'], user_id) for _ in range(num_reposts)])
+
+        return posts, comments, likes, reposts
+
+    def generate_fake_instagram_data(self, user_count=10, posts_per_user=5):
+        users = []
+        all_posts = []
+        all_comments = []
+        all_likes = []
+        all_reposts = []
+
+        for _ in range(user_count):
+            user = self.generate_user()
+            users.append(user)
+
+            user_posts, user_comments, user_likes, user_reposts = self.generate_post_and_interactions(
+                user['user_id'], post_count=posts_per_user)
+
+            all_posts.extend(user_posts)
+            all_comments.extend(user_comments)
+            all_likes.extend(user_likes)
+            all_reposts.extend(user_reposts)
+
+        return users, all_posts, all_comments, all_likes, all_reposts
+
+
+class LinkedInProfile:
+    def __init__(self):
+        self.fake = Faker()
+
+    def generate_user(self):
+        return {
+            "user_id": self.fake.uuid4(),
+            "first_name": self.fake.first_name(),
+            "last_name": self.fake.last_name(),
+            "headline": self.fake.job(),
+            "location": f"{self.fake.city()}, {self.fake.country()}",
+            "summary": self.fake.text(max_nb_chars=300),
+            "profile_image_url": "https://randomfox.ca/",
+            "industry": self.fake.bs(),
+            "education": self.fake.catch_phrase(),
+            "experience": self.fake.text(max_nb_chars=300),
+            "skills": [self.fake.word() for _ in range(5)],
+            "connections": random.randint(50, 5000),
+        }
+
+    def generate_post(self, user_id):
+        return {
+            "post_id": self.fake.uuid4(),
+            "user_id": user_id,
+            "content": self.fake.text(),
+            "likes": random.randint(0, 50000),
+            "comments": random.randint(0, 1000),
+            "shares": random.randint(0, 1000),
+            "created_at": str(self.fake.date_time_between(start_date='-2y', end_date='now')),
+        }
+
+    def generate_comment(self, post_id, user_id):
+        return {
+            "comment_id": self.fake.uuid4(),
+            "post_id": post_id,
+            "user_id": user_id,
+            "text": self.fake.text(max_nb_chars=200),
+            "created_at": str(self.fake.date_time_between(start_date='-2y', end_date='now')),
+        }
+
+    def generate_like(self, post_id, user_id):
+        return {
+            "like_id": self.fake.uuid4(),
+            "post_id": post_id,
+            "user_id": user_id,
+            "created_at": str(self.fake.date_time_between(start_date='-2y', end_date='now')),
+        }
+
+    def generate_share(self, post_id, user_id):
+        return {
+            "share_id": self.fake.uuid4(),
+            "post_id": post_id,
+            "user_id": user_id,
+            "created_at": str(self.fake.date_time_between(start_date='-2y', end_date='now')),
+        }
+
+    def generate_post_and_interactions(self, user_id, post_count=10):
+        posts = []
+        comments = []
+        likes = []
+        shares = []
+
+        for _ in range(post_count):
+            post = self.generate_post(user_id)
+            posts.append(post)
+
+            num_comments = random.randint(0, 10)
+            num_likes = random.randint(0, 100)
+            num_shares = random.randint(0, 50)
+
+            comments.extend([self.generate_comment(post['post_id'], user_id) for _ in range(num_comments)])
+            likes.extend([self.generate_like(post['post_id'], user_id) for _ in range(num_likes)])
+            shares.extend([self.generate_share(post['post_id'], user_id) for _ in range(num_shares)])
+
+        return posts, comments, likes, shares
+
+    def generate_fake_linkedin_data(self, user_count=10, posts_per_user=5):
+        users = []
+        all_posts = []
+        all_comments = []
+        all_likes = []
+        all_shares = []
+
+        for _ in range(user_count):
+            user = self.generate_user()
+            users.append(user)
+
+            user_posts, user_comments, user_likes, user_shares = self.generate_post_and_interactions(
+                user['user_id'], post_count=posts_per_user)
+
+            all_posts.extend(user_posts)
+            all_comments.extend(user_comments)
+            all_likes.extend(user_likes)
+            all_shares.extend(user_shares)
+
+        return users, all_posts, all_comments, all_likes, all_shares
+
+
+# Example usage
+if __name__ == "__main__":
+    # Twitter
+    xprofile = Xprofile()
+    twitter_users, tweets, comments, likes, retweets = xprofile.generate_fake_twitter_data()
+    with open("twitter_data.json", "w") as f:
+        json.dump({
+            "users": twitter_users,
+            "tweets": tweets,
+            "comments": comments,
+            "likes": likes,
+            "retweets": retweets,
+        }, f, indent=4)
+
+    # Instagram
+    insta = InstaProfile()
+    insta_users, posts, insta_comments, insta_likes, insta_reposts = insta.generate_fake_instagram_data()
+    with open("insta_data.json", "w") as f:
+        json.dump({
+            "users": insta_users,
+            "posts": posts,
+            "comments": insta_comments,
+            "likes": insta_likes,
+            "reposts": insta_reposts,
+        }, f, indent=4)
+
+    # LinkedIn
+    linkedin = LinkedInProfile()
+    linkedin_users, linkedin_posts, linkedin_comments, linkedin_likes, linkedin_shares = linkedin.generate_fake_linkedin_data()
+    with open("linkedin_data.json", "w") as f:
+        json.dump({
+            "users": linkedin_users,
+            "posts": linkedin_posts,
+            "comments": linkedin_comments,
+            "likes": linkedin_likes,
+            "shares": linkedin_shares,
+        }, f, indent=4)
